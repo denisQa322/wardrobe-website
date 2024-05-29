56817d5ae9f5c111304f9c972ed1735f2cc72554
denisQa322 <denis.isakov.99@gmail.com>
2024-05-30 00:54:05 +0500

    created styles for devices

--- scripts/changeItems.js
+++ scripts/changeItems.js
@@ -6,20 +6,20 @@ function changeItem(itemType, itemSrc) {
 
 // Функция для обновления canvas
 function updateCanvas() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
 
     // Очищаем canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Получаем все слои в правильном порядке
-    var layersOrder = ['background', 'avatar', 'hat', 'body', 'bottom', 'glasses', 'accessories-left', 'accessories-right'];
+    let layersOrder = ['background', 'avatar', 'hat', 'body', 'bottom', 'glasses', 'accessories-left', 'accessories-right'];
     
     // Рисуем каждый слой на canvas
     layersOrder.forEach(layerId => {
-        var layer = document.getElementById(layerId);
+        let layer = document.getElementById(layerId);
         if (layer && layer.src) {
-            var img = new Image();
+            let img = new Image();
             img.src = layer.src;
             img.onload = function() {
                 ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
@@ -30,11 +30,11 @@ function updateCanvas() {
 
 // Функция для скачивания изображения
 function downloadImage() {
-    var canvas = document.getElementById('canvas');
-    var imageUrl = canvas.toDataURL('image/png');
+    let canvas = document.getElementById('canvas');
+    let imageUrl = canvas.toDataURL('image/png');
 
     // Создаем временную ссылку для скачивания изображения
-    var link = document.createElement('a');
+    let link = document.createElement('a');
     link.href = imageUrl;
     link.download = 'downloaded_image.png';
     document.body.appendChild(link);
